bef2133d4944252b0fedc55c93a0eec01dbd1eda
MahmoudNegm1 <77124033+MahmoudNegm1@users.noreply.github.com>
2022-11-25 10:12:10 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Enter your First Number ");
+int firstNumber = int.Parse(Console.ReadLine());
+Console.WriteLine(  "Enter your second Number " );
+int secondNumber = int.Parse(Console.ReadLine());
+
+int y = firstNumber;
+firstNumber = secondNumber;
+secondNumber = y;
+
+Console.WriteLine($"first number ={firstNumber} \n secondNumber = { secondNumber }");
